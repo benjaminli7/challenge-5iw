fb195a661dda180156439963fae2383ce4466de0
agraval <alexandre.samson78@gmail.com>
2023-12-05 21:04:00 +0100

    rebase to dev

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,10 +1,28 @@
-/* craco.config.js */
-const path = require(`path`);
+const path = require('path');
 
 module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src/"),
+      '@': path.resolve(__dirname, 'src/'),
+    },
+    configure: (webpackConfig) => {
+      if (webpackConfig.cache) {
+        webpackConfig.cache = {
+          type: 'filesystem',
+          buildDependencies: {
+            config: [__filename]
+          }
+        };
+      }
+
+      webpackConfig.watchOptions = {
+        ignored: /node_modules/,
+        aggregateTimeout: 300,
+        poll: 1000
+      };
+
+      return webpackConfig;
     },
   },
 };
