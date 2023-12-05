bac2c8e18d0ffe3bb05ede6e9c45739d6d2d6bd8
agraval <alexandre.samson78@gmail.com>
2023-12-06 00:35:20 +0100

    style Profile

--- frontend/src/pages/Profile/ProfileView.js
+++ frontend/src/pages/Profile/ProfileView.js
@@ -1,18 +1,41 @@
-import { Stack, Typography } from "@mui/material";
-import { useAuthUser } from "react-auth-kit";
+import React from 'react';
+import { useAuthUser } from 'react-auth-kit';
+import { Card, CardContent, Typography, Button, Container, Box } from '@mui/material';
+import { format } from 'date-fns';
 
 function ProfileView() {
-  const auth = useAuthUser();
-  const { email, firstName, lastName, phone } = auth().user;
-  return (
-    <Stack>
-        <Typography variant="h4">Profile</Typography>
-        <Typography variant="h6">Email: {email}</Typography>
-        <Typography variant="h6">First Name: {firstName}</Typography>
-        <Typography variant="h6">Last Name: {lastName}</Typography>
-        <Typography variant="h6">Phone: {phone ?? "Non défini" }</Typography>
-    </Stack>
-  );
+    const auth = useAuthUser();
+    const { email, firstName, lastName, phone, createdAt } = auth().user;
+    const memberSince = createdAt ? format(new Date(createdAt), 'PP') : 'Date not available';
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    mt: 8,
+                }}
+            >
+                <Card sx={{ width: '100%', boxShadow: 3 }}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div" align="center">
+                            Profile
+                        </Typography>
+                        <Typography variant="body1"><b>Email:</b> {email}</Typography>
+                        <Typography variant="body1"><b>First Name:</b> {firstName}</Typography>
+                        <Typography variant="body1"><b>Last Name:</b> {lastName}</Typography>
+                        <Typography variant="body1"><b>Phone:</b> {phone ?? "Not defined"}</Typography>
+                        <Typography variant="body1"><b>Member Since:</b> {memberSince}</Typography>
+                    </CardContent>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', pb: 2 }}>
+                        <Button variant="contained" size="small">Edit Profile</Button>
+                    </Box>
+                </Card>
+            </Box>
+        </Container>
+    );
 }
 
 export default ProfileView;
