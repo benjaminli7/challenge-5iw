a92e2e987bef0a7b0cc6e15c8ea9d9efe5f369b6
Benjamin Li <nicky01.bl@gmail.com>
2023-12-05 12:01:46 +0100

    fix package json

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -2,9 +2,15 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/layout/user/Navbar";
 import Footer from "./components/layout/user/Footer";
 import { Box, CssBaseline, Toolbar } from "@mui/material";
+// import { useAuthUser } from "react-auth-kit";
+// import { Navigate } from "react-router-dom";
 
 function Root() {
-
+  // const auth = useAuthUser();
+  // console.log(auth())
+  // if(auth().user.isFirstConnection === true) {
+  //   return <Navigate to="/profile" replace />;
+  // }
   return (
     <>
       <CssBaseline />
