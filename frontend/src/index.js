ff77c1f7dd00194f34ff846f3c80c1859c0c6fa0
agraval <alexandre.samson78@gmail.com>
2023-12-19 16:36:15 +0100

    Profile editing-cleaning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,7 +13,6 @@ import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
 import Home from "./pages/Home/Home";
 import ProfileView from "./pages/Profile/ProfileView";
-import EditProfile from "@/pages/Profile/EditProfile";
 import Root from "./Root";
 import RootAdmin from "./RootAdmin";
 import theme from "./theme/theme";
@@ -50,10 +49,6 @@ const router = createBrowserRouter([
             <ProfileView />
           </ProtectedRoute>
         ),
-      },
-      {
-        path: "/profile/edit",
-        element: <EditProfile />,
       },
     ],
   },
