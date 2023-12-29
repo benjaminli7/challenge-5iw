f1b9575c8c62bcb1ca0d7679a1be6a6746a1819f
benjaminli7 <nicky01.bl@gmail.com>
2023-12-29 22:50:09 +0100

    test

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import GameView from '../Game/GameView'
+// import GameView from '../Game/GameView'
 function Home() {
   return (
-    <div><GameView /></div>
+    <div>Homepage</div>
   )
 }
 
