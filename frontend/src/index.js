c4e91e833964b0cd171c387fdcd1a2fff8978df5
CamilleBorra <cborra@hotmail.fr>
2024-02-10 15:00:17 +0100

    fix conflit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,15 +20,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
-import AdminDashboardView from "./pages/Admin/AdminDashboardView";
-import AdminUsersView from "./pages/Admin/AdminUsersView";
-import Login from "./pages/auth/Login";
-import Signup from "./pages/auth/Signup";
-import Home from "./pages/Home/Home";
-import ProfileView from "./pages/Profile/ProfileView";
-import Root from "./Root";
-import RootAdmin from "./RootAdmin";
-import theme from "./theme/theme";
+
 import FirstConnection from "./pages/first-connection/FirstConnection";
 const queryClient = new QueryClient();
 
