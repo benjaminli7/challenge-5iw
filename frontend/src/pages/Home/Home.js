12d729903297727b9d5ef68873c8b89f5cb6f577
CamilleBorra <cborra@hotmail.fr>
2024-02-13 10:45:03 +0100

    home page

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,8 +1,60 @@
 import React from "react";
+import { Typography, Button, Grid } from '@material-ui/core';
+import { useIsAuthenticated } from "react-auth-kit";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  title: {
+    fontSize: '3rem',
+    marginBottom: theme.spacing(3),
+    '@media (max-width:600px)': {
+      fontSize: '1.5rem',
+    },
+  },
+  image: {
+    maxWidth: '100%',
+    height: 'auto',
+  },
+  button: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
 function Home() {
+  const classes = useStyles();
+  const isAuthenticated = useIsAuthenticated();
+  console.log("isAuthenticated", isAuthenticated)
+  const handleButtonClick = () => {
+    if (isAuthenticated) {
+      window.location.href = "/client/players";
+    } else {
+      window.location.href = "/login";
+    }
+  };
+
   return (
-    <div>
-      <p> Home Page</p>
+    <div className={classes.root}>
+      <Grid container spacing={3} alignItems="center">
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h1" className={`${classes.title} text-3xl md:text-4xl lg:text-5xl`}>
+            Welcome to Game Elevate, the boosting reservation website.
+          </Typography>
+          <Button variant="contained" color="primary" className={classes.button} onClick={handleButtonClick}>
+            {isAuthenticated ? "List of players" : "Login"}
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <img
+            src="/jeuxvideo.jpg"
+            alt="Jeux vidéo"
+            className={classes.image}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
