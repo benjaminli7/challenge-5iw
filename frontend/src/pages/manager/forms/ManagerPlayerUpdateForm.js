f28e30c1636ca570cfe22a9bb91150fec72a7f4e
syalicheff <yalicheff.sebastien@gmail.com>
2024-02-14 01:29:53 +0100

    Fix Update Form

--- frontend/src/pages/manager/forms/ManagerPlayerUpdateForm.js
+++ frontend/src/pages/manager/forms/ManagerPlayerUpdateForm.js
@@ -1,22 +1,25 @@
 import { useUsers } from "@/hooks/models/useUsers";
 import ManagerPlayerForm from "@/pages/manager/forms/ManagerPlayerForm";
 import { toast } from "sonner";
+import { useState } from "react";
 
 function ManagerPlayerUpdateForm({ selectedUser, handleDialogClose, games }) {
-  const { updateUserMutation, deleteUserMutation } = useUsers(
-    selectedUser.id
-  );
+  const { updateUserMutation, deleteUserMutation } = useUsers(selectedUser.id);
+  const [latLng, setLatLng] = useState({ lat: 0, lng: 0 });
+
   const onSubmit = async (data) => {
     try {
       const dataToSend = {
         ...data,
+        lat: latLng.lat,
+        lng: latLng.lng,
         assignedGame: `/api/games/${data.assignedGame}`,
-      }
+      };
       await updateUserMutation.mutateAsync(dataToSend);
       await handleDialogClose();
       toast.success("Player updated!");
     } catch (error) {
-      console.log(error)
+      console.log(error);
       console.error("Error updating player:", error);
       toast.error("Error updating player");
     }
@@ -39,6 +42,7 @@ function ManagerPlayerUpdateForm({ selectedUser, handleDialogClose, games }) {
       onSubmit={onSubmit}
       handleDeletePlayer={handleDeletePlayer}
       games={games}
+      setLatLng={setLatLng}
     />
   );
 }
